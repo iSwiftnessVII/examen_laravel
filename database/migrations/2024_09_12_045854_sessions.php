a2c052ca66ec19cd096e64683542a79b7440999b
iSwiftnessVII <blacknighthappy@gmail.com>
2024-09-12 00:56:07 -0500

    Update 2024_09_12_045854_sessions.php

--- database/migrations/2024_09_12_045854_sessions.php
+++ database/migrations/2024_09_12_045854_sessions.php
@@ -12,10 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps(); // campos de fecha de inicio (created_at) y alta (updated_at)
+            $table->string('id')->primary();               
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
         });
     }
+    
 
     /**
      * Reverse the migrations.
